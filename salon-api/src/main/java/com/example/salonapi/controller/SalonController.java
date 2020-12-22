2658d53287e1c8fc7b659532eede9db563c54732
Andrej Knezevski <knezevskia@gmail.com>
2020-12-22 00:53:39 -0500

    Add nav bar and entities (#2)
    * Add Repositories and React navbar
    
    * rm .eslintcache
    
    Co-authored-by: Andrej Knezevski <aknezevski@zynga.com>

---
+++ salon-api/src/main/java/com/example/salonapi/controller/SalonController.java
@@ -0,0 +1,48 @@
+package com.example.salonapi.controller;
+
+import com.example.salonapi.SalonServiceDetail;
+import com.example.salonapi.Slot;
+import com.example.salonapi.repository.SalonServiceDetailRepository;
+import com.example.salonapi.repository.SlotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/salon", produces = "application/json")
+public class SalonController {
+    private final SlotRepository slotRepository;
+    private final SalonServiceDetailRepository salonServiceDetailRepository;
+
+    @Autowired
+    public SalonController(SlotRepository slotRepository,
+                           SalonServiceDetailRepository salonServiceDetailRepository) {
+        this.slotRepository = slotRepository;
+        this.salonServiceDetailRepository = salonServiceDetailRepository;
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "There is an error!";
+    }
+
+    @GetMapping("/slots")
+    public List<Slot> getSlots() {
+        List<Slot> slots = new ArrayList<>();
+
+        this.slotRepository.findAll().forEach(slots::add);
+        return slots;
+    }
+
+    @GetMapping("/salonServiceDetails")
+    public List<SalonServiceDetail> getSalonServiceDetails() {
+        List<SalonServiceDetail> salonServiceDetails = new ArrayList<>();
+
+        this.salonServiceDetailRepository.findAll().forEach(salonServiceDetails::add);
+        return salonServiceDetails;
+    }
+}
---
+++ salon-api/src/main/java/com/example/salonapi/repository/SalonServiceDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.salonapi.repository;
+
+import com.example.salonapi.SalonServiceDetail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalonServiceDetailRepository extends CrudRepository<SalonServiceDetail, Long> {
+}
